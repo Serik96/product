102f3955f2b10330c63d00fea6a78b37e635bc80
Serik <s.kappar@aster.kz>
2023-12-06 15:58:11 +0600

    add: eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import { ThemeProvider } from "app/providers/ThemeProvider";
 import App from "./app/App";
+import "./shared/config/i18n/i18n";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./shared/config/i18n/i18n";
+import { ThemeProvider } from "app/providers/ThemeProvider";
 
 render(
   <BrowserRouter>
